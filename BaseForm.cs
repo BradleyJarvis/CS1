245c2e02ec6798f48d527fef79724804a2888f7d
SBurwell <stephen.burwell.94@gmail.com>
2015-02-26 23:03:01 +0000

    Initial prototype
    Created initial UI prototype, including inheritance and one example of
    each type of form

---
+++ BaseForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Group_project
+{
+    public partial class BaseForm : Form
+    {//This form contains anything that all forms will use, such as the menu strip and returning to main menu when closed
+        public BaseForm()
+        {
+            InitializeComponent();
+        }
+
+        private void formClose(object sender, FormClosedEventArgs e)
+        {
+            MainMenuForm mainMenu = new MainMenuForm();
+            mainMenu.Show();
+        }
+
+        private void exitProgram(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
