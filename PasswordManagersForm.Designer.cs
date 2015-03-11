008893689f4ac5e3a848b3374fc9ecddeb8c7fd1
Stephen Burwell <stephen.burwell.94@gmail.com>
2015-03-11 15:03:42 +0000

    Password generator added
    Added a simple password generator to the password advice section

--- PasswordManagersForm.Designer.cs
+++ PasswordManagersForm.Designer.cs
@@ -28,9 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.passOutputBox = new System.Windows.Forms.TextBox();
+            this.passLengthBox = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.enPassButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.passLengthBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // passOutputBox
+            // 
+            this.passOutputBox.Location = new System.Drawing.Point(14, 97);
+            this.passOutputBox.Name = "passOutputBox";
+            this.passOutputBox.ReadOnly = true;
+            this.passOutputBox.Size = new System.Drawing.Size(120, 20);
+            this.passOutputBox.TabIndex = 8;
+            // 
+            // passLengthBox
+            // 
+            this.passLengthBox.Location = new System.Drawing.Point(14, 33);
+            this.passLengthBox.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.passLengthBox.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.passLengthBox.Name = "passLengthBox";
+            this.passLengthBox.Size = new System.Drawing.Size(44, 20);
+            this.passLengthBox.TabIndex = 9;
+            this.passLengthBox.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Password length";
+            // 
+            // enPassButton
+            // 
+            this.enPassButton.Location = new System.Drawing.Point(14, 59);
+            this.enPassButton.Name = "enPassButton";
+            this.enPassButton.Size = new System.Drawing.Size(120, 23);
+            this.enPassButton.TabIndex = 11;
+            this.enPassButton.Text = "Generate password";
+            this.enPassButton.UseVisualStyleBackColor = true;
+            this.enPassButton.Click += new System.EventHandler(this.genPassword);
+            // 
+            // PasswordManagersForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(453, 314);
+            this.Controls.Add(this.enPassButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passLengthBox);
+            this.Controls.Add(this.passOutputBox);
+            this.Name = "PasswordManagersForm";
+            this.Controls.SetChildIndex(this.passOutputBox, 0);
+            this.Controls.SetChildIndex(this.passLengthBox, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.enPassButton, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.passLengthBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox passOutputBox;
+        private System.Windows.Forms.NumericUpDown passLengthBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button enPassButton;
     }
 }
--- PasswordManagersForm.cs
+++ PasswordManagersForm.cs
@@ -10,9 +10,23 @@ namespace Group_project
 {
     public partial class PasswordManagersForm : Group_project.InteractionBase
     {
+        //characters array used to hold all characters that can be chosen for the generated password
+        char[] characters = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        Random rnd = new Random(); //Create the RNG object
+
         public PasswordManagersForm()
         {
             InitializeComponent();
+        }
+
+        private void genPassword(object sender, EventArgs e)
+        {
+            string password = "";
+            for (int count = 0; count < passLengthBox.Value; count++)//Run as many times as needed to create password of desired length
+            {//Choose a character at random from the character list and add it to the generated password
+                password += characters[rnd.Next(0, characters.Length)];
+            }
+            passOutputBox.Text = password;
         }
     }
 }
