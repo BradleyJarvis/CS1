4fd866710ace83fe833a477abd42298c59b781f2
Stephen Burwell <stephen.burwell.94@gmail.com>
2015-03-26 14:55:45 +0000

    Quiz prototype made
    The early prototype for the quiz has been created, including
    functionality for multiple questions, answer checking and progressing to
    the next question.

--- QuizForm.Designer.cs
+++ QuizForm.Designer.cs
@@ -29,8 +29,110 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.questionDisplayText = new System.Windows.Forms.TextBox();
+            this.answerButton1 = new System.Windows.Forms.Button();
+            this.answerButton2 = new System.Windows.Forms.Button();
+            this.answerButton3 = new System.Windows.Forms.Button();
+            this.answerButton4 = new System.Windows.Forms.Button();
+            this.correctLabel = new System.Windows.Forms.Label();
+            this.nextQuestionTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // questionDisplayText
+            // 
+            this.questionDisplayText.Location = new System.Drawing.Point(13, 28);
+            this.questionDisplayText.Multiline = true;
+            this.questionDisplayText.Name = "questionDisplayText";
+            this.questionDisplayText.ReadOnly = true;
+            this.questionDisplayText.Size = new System.Drawing.Size(526, 97);
+            this.questionDisplayText.TabIndex = 8;
+            this.questionDisplayText.Text = "Questions are shown here";
+            // 
+            // answerButton1
+            // 
+            this.answerButton1.Location = new System.Drawing.Point(13, 132);
+            this.answerButton1.Name = "answerButton1";
+            this.answerButton1.Size = new System.Drawing.Size(127, 80);
+            this.answerButton1.TabIndex = 9;
+            this.answerButton1.Text = "button1";
+            this.answerButton1.UseVisualStyleBackColor = true;
+            this.answerButton1.Click += new System.EventHandler(this.answerButton1_Click);
+            // 
+            // answerButton2
+            // 
+            this.answerButton2.Location = new System.Drawing.Point(146, 132);
+            this.answerButton2.Name = "answerButton2";
+            this.answerButton2.Size = new System.Drawing.Size(127, 80);
+            this.answerButton2.TabIndex = 10;
+            this.answerButton2.Text = "button2";
+            this.answerButton2.UseVisualStyleBackColor = true;
+            this.answerButton2.Click += new System.EventHandler(this.answerButton2_Click);
+            // 
+            // answerButton3
+            // 
+            this.answerButton3.Location = new System.Drawing.Point(279, 132);
+            this.answerButton3.Name = "answerButton3";
+            this.answerButton3.Size = new System.Drawing.Size(127, 80);
+            this.answerButton3.TabIndex = 11;
+            this.answerButton3.Text = "button3";
+            this.answerButton3.UseVisualStyleBackColor = true;
+            this.answerButton3.Click += new System.EventHandler(this.answerButton3_Click);
+            // 
+            // answerButton4
+            // 
+            this.answerButton4.Location = new System.Drawing.Point(412, 131);
+            this.answerButton4.Name = "answerButton4";
+            this.answerButton4.Size = new System.Drawing.Size(127, 80);
+            this.answerButton4.TabIndex = 12;
+            this.answerButton4.Text = "button4";
+            this.answerButton4.UseVisualStyleBackColor = true;
+            this.answerButton4.Click += new System.EventHandler(this.answerButton4_Click);
+            // 
+            // correctLabel
+            // 
+            this.correctLabel.AutoSize = true;
+            this.correctLabel.Location = new System.Drawing.Point(13, 232);
+            this.correctLabel.Name = "correctLabel";
+            this.correctLabel.Size = new System.Drawing.Size(35, 13);
+            this.correctLabel.TabIndex = 13;
+            this.correctLabel.Text = "label1";
+            this.correctLabel.Visible = false;
+            // 
+            // nextQuestionTimer
+            // 
+            this.nextQuestionTimer.Interval = 2000;
+            this.nextQuestionTimer.Tick += new System.EventHandler(this.nextQuestionTimer_Tick);
+            // 
+            // QuizForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(552, 316);
+            this.Controls.Add(this.correctLabel);
+            this.Controls.Add(this.answerButton4);
+            this.Controls.Add(this.answerButton3);
+            this.Controls.Add(this.answerButton2);
+            this.Controls.Add(this.answerButton1);
+            this.Controls.Add(this.questionDisplayText);
+            this.Name = "QuizForm";
+            this.Controls.SetChildIndex(this.questionDisplayText, 0);
+            this.Controls.SetChildIndex(this.answerButton1, 0);
+            this.Controls.SetChildIndex(this.answerButton2, 0);
+            this.Controls.SetChildIndex(this.answerButton3, 0);
+            this.Controls.SetChildIndex(this.answerButton4, 0);
+            this.Controls.SetChildIndex(this.correctLabel, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox questionDisplayText;
+        private System.Windows.Forms.Button answerButton1;
+        private System.Windows.Forms.Button answerButton2;
+        private System.Windows.Forms.Button answerButton3;
+        private System.Windows.Forms.Button answerButton4;
+        private System.Windows.Forms.Label correctLabel;
+        private System.Windows.Forms.Timer nextQuestionTimer;
     }
 }
--- QuizForm.cs
+++ QuizForm.cs
@@ -5,14 +5,121 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Linq;
 
 namespace Group_project
 {
     public partial class QuizForm : Group_project.InteractionBase
     {
+        Random rnd = new Random();// Create an RNG for shuffling the order answers appear
+        int currentQuestion = 0;
+        List <question> allQuestions = new List <question>();
+
         public QuizForm()
         {
             InitializeComponent();
+
+            //This is the start of test code, it should all be removed once finished
+            question Q1 = new question ("What is the answer to this question", "This is correct", "Answer 2", "Answer 3", "Answer 4");
+            allQuestions.Add(Q1);
+            question Q2 = new question ("This is the second question, used to check the functionality", "This is the correct answer", "This is wrong", "This is wrong too", "And this is wrong as well");
+            allQuestions.Add(Q2);
+            question Q3 = new question("This is the third and final test question", "This answer is correct", "This answer is not correct", "This is a wrong answer", "And this is wrong too");
+            allQuestions.Add(Q3);
+            showNewQuestion(allQuestions[0]);
+            //End of test code
+        }
+
+        void showNewQuestion(question Q)
+        {
+            questionDisplayText.Text = Q.questionText;
+
+            int[] answersPlaced = new int[] {5,5,5,5};// Array used to track what answers have been placed on buttons already, starts containing 5s to prevent hang when 0 chosen
+            int number = 0;// Used to store the random number while it is check that it isn't already placed
+            Button[] buttons = new Button[] {answerButton1, answerButton2, answerButton3, answerButton4};
+            string[] answers = new string[] {Q.answer1, Q.answer2, Q.answer3, Q.answer4};// Aray used to hold strings with the possible answers
+            for (int count = 0; count < 4; count++)
+            {// Place a random answer on button[count]
+                number = rnd.Next(0, 4);
+                if (answersPlaced.Contains(number) == true)
+                    count--;
+                else
+                {
+                    answersPlaced[count] = number;
+                    buttons[count].Text = answers[number];
+                }
+            }
+        }
+
+        bool checkAnswer(Button buttonPressed)
+        {// Checks the answer given, if it is core=rect it will return true, else it weill return false
+            if (buttonPressed.Text == allQuestions[currentQuestion].answer1)
+                return true;
+            else
+                return false;
+        }
+
+        void updateResults(Button buttonPressed)
+        {// Used to update quiz progress systems and move to next question
+            // Update the correct label as needed
+            if (checkAnswer(buttonPressed) == true)
+                correctLabel.Text = "True";
+            else
+                correctLabel.Text = "False";
+            correctLabel.Show();
+
+            nextQuestionTimer.Enabled = true;
+        }
+        #region answerButtonClick
+        private void answerButton1_Click(object sender, EventArgs e)
+        {
+            updateResults(answerButton1);
+        }
+
+
+        private void answerButton2_Click(object sender, EventArgs e)
+        {
+            updateResults(answerButton2);
+        }
+
+        private void answerButton3_Click(object sender, EventArgs e)
+        {
+            updateResults(answerButton3);
+        }
+
+        private void answerButton4_Click(object sender, EventArgs e)
+        {
+            updateResults(answerButton4);
+        }
+        #endregion
+
+        private void nextQuestionTimer_Tick(object sender, EventArgs e)
+        {
+            nextQuestionTimer.Enabled = false;
+            correctLabel.Hide();
+            currentQuestion++;
+
+            if (currentQuestion >= allQuestions.Count)
+            {
+                MessageBox.Show("This is the end of the quiz");
+                return;
+            }
+
+            showNewQuestion(allQuestions[currentQuestion]);
+        }
+    }
+
+    public class question
+    {
+        public string questionText, answer1, answer2, answer3, answer4;// One string used to hold the text asking the question, 4 more used for each of the possible answers. answer1 should be used to hold the correct answer, all other should be incorrect
+
+        public question (string Q, string a1, string a2, string a3, string a4)
+        {// Constructor used to assign strings to correct variables
+            questionText = Q;
+            answer1 = a1;
+            answer2 = a2;
+            answer3 = a3;
+            answer4 = a4;
         }
     }
 }
