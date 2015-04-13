7fc2189abecc077ced900b2ea5d6b8716c0ebc2d
Stephen Burwell <stephen.burwell.94@gmail.com>
2015-04-14 00:32:32 +0100

    Fixed error introduced in previous commit
    Previous commit introduced error in Public Wifi section, now fixed

--- PublicWifiForm.Designer.cs
+++ PublicWifiForm.Designer.cs
@@ -28,8 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-<<<<<<< HEAD
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PublicWifiForm));
             this.TitleLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // TitleLabel
@@ -37,23 +40,17 @@
             this.TitleLabel.AutoSize = true;
             this.TitleLabel.Font = new System.Drawing.Font("Tahoma", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TitleLabel.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.TitleLabel.Location = new System.Drawing.Point(-5, 24);
+            this.TitleLabel.Location = new System.Drawing.Point(4, 24);
             this.TitleLabel.Name = "TitleLabel";
             this.TitleLabel.Size = new System.Drawing.Size(123, 25);
             this.TitleLabel.TabIndex = 44;
             this.TitleLabel.Text = "Public Wifi";
-=======
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PublicWifiForm));
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(12, 29);
+            this.textBox1.Location = new System.Drawing.Point(12, 51);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
@@ -65,30 +62,24 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Group_project.Properties.Resources.PubWifi;
-            this.pictureBox1.Location = new System.Drawing.Point(411, 272);
+            this.pictureBox1.Location = new System.Drawing.Point(425, 291);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(405, 213);
             this.pictureBox1.TabIndex = 10;
             this.pictureBox1.TabStop = false;
->>>>>>> origin/master
             // 
             // PublicWifiForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-<<<<<<< HEAD
-            this.ClientSize = new System.Drawing.Size(664, 261);
+            this.ClientSize = new System.Drawing.Size(843, 510);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.TitleLabel);
             this.Name = "PublicWifiForm";
             this.Controls.SetChildIndex(this.TitleLabel, 0);
-=======
-            this.ClientSize = new System.Drawing.Size(843, 487);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox1);
-            this.Name = "PublicWifiForm";
             this.Controls.SetChildIndex(this.textBox1, 0);
             this.Controls.SetChildIndex(this.pictureBox1, 0);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
->>>>>>> origin/master
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -96,11 +87,8 @@
 
         #endregion
 
-<<<<<<< HEAD
         private System.Windows.Forms.Label TitleLabel;
-=======
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
->>>>>>> origin/master
     }
 }
