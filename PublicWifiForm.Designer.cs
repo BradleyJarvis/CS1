0e3f3c458fc85009479b5eeb1bab9aea2a08aaf4
Stephen Burwell <stephen.burwell.94@gmail.com>
2015-04-15 11:08:09 +0100

    Added whitelist and encryption section
    Added the whitelists and encryption section written by Daniel. Also
    changed the text box on public wifi to match style on other pages

--- PublicWifiForm.Designer.cs
+++ PublicWifiForm.Designer.cs
@@ -48,6 +48,7 @@
             // 
             // textBox1
             // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(12, 51);
