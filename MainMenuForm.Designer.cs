678505110c8d21182c84001eab74e3488066d5ba
Stephen Burwell <stephen.burwell.94@gmail.com>
2015-04-15 10:52:07 +0100

    Added quiz questions
    Added another 2 questions to the quiz. Also removed the default app icon
    from all windows

--- MainMenuForm.Designer.cs
+++ MainMenuForm.Designer.cs
@@ -492,6 +492,7 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MainMenuForm";
+            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Edu-cyber scelus";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.mainMenuClose);
